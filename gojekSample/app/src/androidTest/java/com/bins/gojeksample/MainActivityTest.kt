9355f645aab13a2fb34710ee15628870095a4502
Binil Thomas <binil@vervone.com>
2020-02-18 23:55:27 +0530

    Base setup, UI and Test

--- gojekSample/app/src/androidTest/java/com/bins/gojeksample/MainActivityTest.kt
+++ gojekSample/app/src/androidTest/java/com/bins/gojeksample/MainActivityTest.kt
@@ -1,12 +1,19 @@
 package com.bins.gojeksample
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +21,23 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4ClassRunner::class)
-class ExampleInstrumentedTest {
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    public val main = ActivityTestRule(MainActivity::class.java, true)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.bins.gojeksample", appContext.packageName)
     }
+
+    @Test
+    fun testActionBarText(){
+        var  expectedResult :String = getApplicationContext<Context>().getString(R.string.app_name);
+        onView(withId(R.id.toolbar_title)).check(matches(withText(expectedResult)));
+    }
+
 }
